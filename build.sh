91e6eb47685997b819756ab4d38ec2916bf73e02
seasea128 <seasea@windowslive.com>
2025-01-21 08:42:06 +0000

    Remove swagger

--- build.sh
+++ build.sh
@@ -8,6 +8,4 @@ then
 		file_ext=".exe"
 fi
 
-swag init -o ./swagger
-
-go build -o ./bin/WebAPI-$version-$hash_str$file_ext -ldflags "-X main.hash=$hash_str -X main.version=$version"
+go build -v -o ./bin/WebAPI-$version-$hash_str$file_ext #-ldflags "-X main.hash=$hash_str -X main.version=$version"
