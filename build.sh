df8045db69ad4be9b3835cf8a634b16558fa83b1
seasea128 <seasea@windowslive.com>
2025-01-23 21:32:01 +0000

    Add protobuf message

--- build.sh
+++ build.sh
@@ -8,4 +8,9 @@ then
 		file_ext=".exe"
 fi
 
+PROTO_SRC_DIR="./Protobuf-FYP/proto"
+DST_DIR="."
+
+protoc -I=$PROTO_SRC_DIR --go_out=$DST_DIR $PROTO_SRC_DIR/*
+
 go build -v -o ./bin/WebAPI-$version-$hash_str$file_ext #-ldflags "-X main.hash=$hash_str -X main.version=$version"
