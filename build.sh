db73eb1b5e6ce57fd77eb836a99b7c598452d287
seasea128 <seasea@windowslive.com>
2025-02-20 21:18:58 +0000

    Call protoc on only .proto files and verbose build log

--- build.sh
+++ build.sh
@@ -11,14 +11,15 @@ fi
 if [ "$1" = "docker" ]
 then
 	file_name="WebAPI"
+	# export CGO_ENABLED=0
 else
 	file_name="WebAPI-$version-$hash_str$file_ext"
-	export CGO_ENABLED=0
 fi
 
 PROTO_SRC_DIR="./Protobuf-FYP/proto"
 DST_DIR="."
 
-protoc -I=$PROTO_SRC_DIR --go_out=$DST_DIR $PROTO_SRC_DIR/*
+protoc -I=$PROTO_SRC_DIR --go_out=$DST_DIR $PROTO_SRC_DIR/data.proto
+protoc -I=$PROTO_SRC_DIR --go_out=$DST_DIR $PROTO_SRC_DIR/timestamp.proto
 
-go build -o ./bin/$file_name -ldflags "-X main.hash=$hash_str -X main.version=$version"
+go build -v -o ./bin/$file_name -ldflags "-X main.hash=$hash_str -X main.version=$version"
